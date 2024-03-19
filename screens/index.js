1c3127431da0758c74592bead102da36d9a514bf
Rahul Bansal <42.rahulbansal@gmail.com>
2024-03-20 01:14:34 +0530

    Boilerplate screens

---
+++ screens/index.js
@@ -0,0 +1,8 @@
+import Home from './Home';
+import Login from './Login';
+import SignUp from './SignUp';
+import Profile from './Profile';
+import JobPage from './JobPage';
+import CompanyPage from './CompanyPage';
+
+export {Home, Login, SignUp, Profile, JobPage, CompanyPage};
