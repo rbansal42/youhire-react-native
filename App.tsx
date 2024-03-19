8c5bfe3728c4df59834beaa7467f97c552554849
Rahul Bansal <42.rahulbansal@gmail.com>
2024-03-20 01:15:05 +0530

    imports, and styling

--- App.tsx
+++ App.tsx
@@ -1,12 +1,31 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator as Stack} from '@react-navigation/native-stack';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+
+import {Login, SignUp} from './screens';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <View style={styles.appBackground}>
+          {/* <Login /> */}
+          <SignUp />
+        </View>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  appBackground: {
+    flex: 1,
+
+    padding: 24,
+
+    backgroundColor: 'black',
+  },
+});
