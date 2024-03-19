970d24de0803d1f691952ef23c6516449a6eafcc
Rahul Bansal <42.rahulbansal@gmail.com>
2024-03-20 01:13:10 +0530

    Added reactnativeelements library

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 /**
  * @format
  */
+<style type="text/css">{`
+  @font-face {
+    font-family: 'MaterialIcons';
+    src: url(${require('react-native-vector-icons/Fonts/MaterialIcons.ttf')}) format('truetype');
+  }
+
+  @font-face {
+    font-family: 'FontAwesome';
+    src: url(${require('react-native-vector-icons/Fonts/FontAwesome.ttf')}) format('truetype');
+  }
+`}</style>;
 
 import {AppRegistry} from 'react-native';
 import App from './App';
